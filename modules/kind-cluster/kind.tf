1dffebbc8b91efb0aa11dbb3087bbf30472f8ea9
Yurii Bielodied <ybeloded@gmail.com>
2026-03-15 19:50:40 +0200

    feat: update kind module, add worker nodes labeling

--- modules/kind-cluster/kind.tf
+++ modules/kind-cluster/kind.tf
@@ -8,5 +8,29 @@ terraform {
 
 resource "kind_cluster" "kind" {
   name           = "kind-cluster"
+  node_image     = "kindest/node:v1.35.1"
   wait_for_ready = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+    node {
+      role = "control-plane"
+    }
+    node {
+      role = "worker"
+    }
+    node {
+      role = "worker"
+    }
+  }
+}
+
+resource "null_resource" "label_workers" {
+  depends_on = [kind_cluster.kind]
+
+  provisioner "local-exec" {
+    command     = "kubectl get nodes -o name | Select-String 'worker' | ForEach-Object { kubectl label $_ 'node-role.kubernetes.io/worker=true' --overwrite }"
+    interpreter = ["pwsh", "-Command"]
+  }
 }
